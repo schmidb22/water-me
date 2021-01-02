8bb14fab24d96a24714e2114890dce367abfb8bd
schmidb22 <schmidb@mcmaster.ca>
2021-01-01 20:20:46 -0500

    Updating interrupt service routine- added variable declarations

--- water_me/water_me.ino
+++ water_me/water_me.ino
@@ -1,18 +1,26 @@
+my_time = 0;
+time_offset = 216000000; // 2.5 days in milliseconds
+
 void setup() {
   
 }
-my_time = millis();
+
 void loop() {
   
 
 }
 
 void interrupt(){
+  
   time_delay = millis();
+  
   if(time_delay-my_time>172800000){ // Time since last call
+    
     digitalWrite(2,HIGH); // Turn on pump
     delay(3000);
     digitalWrite(2,LOW); // Turn off pump after 3 sec
     my_time = millis(); // Resets value after each watering
+    
   }
+  
 }
