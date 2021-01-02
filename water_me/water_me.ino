844ef524345f6f1f7641481d6ce9fee2c58953d3
mmione <mattmione37@gmail.com>
2021-01-01 20:03:56 -0500

    Updating setup function for proposed setup

--- water_me/water_me.ino
+++ water_me/water_me.ino
@@ -1,5 +1,10 @@
 void setup() {
+
+  // Setting up pins
+  pinMode(2, OUTPUT); // Pin D2 will control relay, will go to pin IN1 on the relay\
   
+  pinMode(3, INPUT); // Setting up interrupt pin
+  attachInterrupt(3, interrupt, RISING); // Should trigger (hopefully) once in the morning
 
 }
 
