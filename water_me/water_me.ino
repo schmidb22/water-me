efb259ff13c661c18245a55f469ac1be8a6ddcae
schmidb22 <schmidb@mcmaster.ca>
2021-01-01 19:55:34 -0500

    Prototyped interrupt service routine

--- water_me/water_me.ino
+++ water_me/water_me.ino
@@ -1,9 +1,18 @@
 void setup() {
   
-
 }
-
+my_time = millis();
 void loop() {
   
 
+}
+
+void interrupt(){
+  time_delay = millis();
+  if(time_delay-my_time>172800000){ // Time since last call
+    digitalWrite(2,HIGH); // Turn on pump
+    delay(3000);
+    digitalWrite(2,LOW); // Turn off pump after 3 sec
+    my_time = millis(); // Resets value after each watering
+  }
 }
